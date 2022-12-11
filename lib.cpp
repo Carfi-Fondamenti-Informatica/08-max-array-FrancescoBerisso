bc4aef84d612373d829c0fd20cf36d14c598c7c4
FrancescoBerisso <115414198+FrancescoBerisso@users.noreply.github.com>
2022-12-11 02:05:58 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
+#include "lib.h"
+float maxArray(float array[], int d){
+    for(int i = 0; i < d; ++i){
+        if(array[0] > array[i]){
 
+        }
+        else{
+            array[0] = array[i];
+        }
+    }
+    return array[0];
+}
